8a5cb7f1cfdc4fb1d8e087a2fe2f7b330e238956
ysb4642 <ysb4642@naver.com>
2021-12-11 18:53:41 +0900

    21_12_11(2)

--- 8단계_기본수학1/add.c
+++ 8단계_기본수학1/add.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+
+void reverse(char arr[])
+{
+	int len, i;
+	char temp;
+
+	len = strlen(arr);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = arr[i];
+		arr[i] = arr[len - 1 - i];
+		arr[len - 1 - i] = temp;
+	}
+}
 
 int main(void)
 {
-	char n1[20];
-	char n2[20];
+	char a[10001] = {0, };
+	char b[10001] = {0, };
+	char res[10002] = {0, };
+	int d; // 자릿수 올림
+	int len, i, sum;
 
-	scanf("%s %s", n1, n2);
-	printf("%s\n", n1);
-	printf("%s\n", n2);
+	scanf("%s %s", a, b);
+	reverse(a);
+	reverse(b);
+	len = strlen(a) >strlen(b) ? strlen(a) : strlen(b);
+	d = 0;
+	for (i = 0; i < len; i++)
+	{
+		sum = (a[i] - 48) + (b[i] - 48) + d;
+		if (sum < 0)
+			sum += 48;		// 자릿수가 다를경우에 sum은 -48을 한번더 계산되기 때문에 (둘중 하나는 null이므로)
+		if (sum > 9)
+			d = 1;
+		else
+			d = 0;
+		res[i] = sum % 10 + 48;
+	}
+	if (d == 1)
+		res[len] = '1';
+	reverse(res);
+	printf("%s", res);
 	return (0);
 }
-
-/*9223372036854775807 9223372036854775808 */
