b6ee7a37b594b283a2a4c8396bf11e701c837fd7
ysb4642 <ysb4642@naver.com>
2022-01-09 20:23:42 +0900

    21_01_09

--- algorithm_1/reverse_word_2.c
+++ algorithm_1/reverse_word_2.c
@@ -1,45 +1,97 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define TRUE	1
+#define FALSE	0
+#define STACK_LEN	100000
+
+typedef struct _arrayStack
+{
+	char stackArr[STACK_LEN];
+	int topIndex;
+} Stack;
+
+void StackInit(Stack *pstack)
+{
+	pstack->topIndex = -1;
+}
+
+int SIsEmpty(Stack *pstack)
+{
+	if (pstack->topIndex == -1)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack *pstack, char data)
+{
+	pstack->topIndex += 1;
+	pstack->stackArr[pstack->topIndex] = data;
+}
+
+char SPop(Stack *pstack)
+{
+	int rIdx;
+
+	if (SIsEmpty(pstack))
+		exit(-1);
+
+	rIdx = pstack->topIndex;
+	pstack->topIndex -= 1;
+
+	return pstack->stackArr[rIdx];
+}
+
+char SPeek(Stack *pstack)
+{
+	if (SIsEmpty(pstack))
+		exit(-1);
+
+	return pstack->stackArr[pstack->topIndex];
+}
+
+
+void stackprint(Stack *pstack)
+{
+	while (!SIsEmpty(pstack))
+	{
+		printf("%c", SPeek(pstack));
+		SPop(pstack);
+	}
+}
 
 int main(void)
 {
 	char str[100000];
-	char stack[100000];
-	int i, j, k, len;
+	Stack stk;
+	int i, len;
+	int tag = FALSE;
+
+	StackInit(&stk);
 
 	fgets(str, 100000, stdin);
 	len = strlen(str) - 1;
-
-	j = 0;
-	k = 0;
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == '<')
 		{
-			if (j == 0)
-				stack[j++] = str[i];
-			else
-			
+			stackprint(&stk);
+			while(1)
+			{
+				printf("%c", str[i]);
+				if (str[i] == '>')
+					break ;
+				i++;
+			}
 		}
-		else if (str[i] == '>')
+		else if (str[i] == ' ' || i == len)
 		{
-			while(stack[k] != '>')
-				printf("%c", stack[k++]);
-			printf("%c", str[i]);
-			k = 0;
+			stackprint(&stk);
+			printf(" ");
 		}
-		else if (str[i] != ' ')
-			stack[j++] = str[i];
-		else if (str[i] == ' ' || i == len - 1)
-		{
-			while(--j >= 0)
-				printf("%c", stack[j]);
-			if (str[i] == ' ')
-				printf(" \n");
-			else
-				printf("\n");
-			j = 0;
-		}
+		else
+			SPush(&stk, str[i]);
 	}
 	return (0);
 }
