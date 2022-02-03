c20d15481d3def8afcc3c32d4f4be4e6c1d3f3e5
ysb4642 <ysb4642@naver.com>
2022-02-03 14:38:26 +0900

    22_02_03

---
+++ algorithm_1/structure_1/itoa.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+int	ft_n_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	long	num;
+	char	*str;
+
+	len = ft_n_len(n);
+	num = n;
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	if (num < 0)
+	{
+		str[0] = '-';
+		num *= -1;
+	}
+	if (num == 0)
+		str[0] = '0';
+	str[len--] = '\0';
+	while (num)
+	{
+		str[len] = num % 10 + '0';
+		len--;
+		num /= 10;
+	}
+	return (str);
+}
