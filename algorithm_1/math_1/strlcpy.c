1a9873cca85cc20557e517266dde1087be1c28a8
ysb4642 <ysb4642@naver.com>
2022-02-05 13:14:22 +0900

    22_02_05

---
+++ algorithm_1/math_1/strlcpy.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize < dst_len + 1)
+		return (dstsize + src_len);
+	while (src[i] != '\0' && dstsize > dst_len + 1 + i)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
