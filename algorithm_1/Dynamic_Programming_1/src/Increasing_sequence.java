86de989a4000528621ef90bf3102d8b1ec69e9fe
ysb4642 <ysb4642@naver.com>
2022-02-26 12:24:37 +0900

    22_02_26

---
+++ algorithm_1/Dynamic_Programming_1/src/Increasing_sequence.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Increasing_sequence {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int[] dp = new int[n + 1];
+		int[] arr = new int[n + 1];
+		
+		for (int i = 1; i <= n; i++)
+			arr[i] = s.nextInt();
+		
+		dp[1] = arr[1];
+		
+		for (int i = 2; i <= n; i++) {
+			dp[i] = arr[i];
+			for (int j = 1; j < i; j++) {
+				if (arr[j] < arr[i])
+					dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+			}
+		}
+		int max = dp[1];
+		for (int i= 2; i <= n; i++) {
+			if (dp[i] > max)
+				max = dp[i];
+		}
+		System.out.println(max);
+	}
+
+}
