9028586f4039eee9c99e86ff74822e305f58d459
ysb4642 <ysb4642@naver.com>
2022-03-09 12:37:29 +0900

    22_03_09

---
+++ brute_force/src/Continuing_num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Continuing_num {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int cnt = 1;
+		int len = 10;
+		int tmp = 0;
+		
+		for(int i = 1; i <= num; i++) {
+			if (i == len) {
+				cnt++;
+				len *= 10;
+			}
+			tmp += cnt;
+		}
+		System.out.println(tmp);
+	}
+}
