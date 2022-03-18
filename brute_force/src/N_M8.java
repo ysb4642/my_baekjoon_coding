63d3f10caae2da17e31e95f4bbbc201ce7d3e640
ysb4642 <ysb4642@naver.com>
2022-03-18 12:50:03 +0900

    22_03_18

--- brute_force/src/N_M8.java
+++ brute_force/src/N_M8.java
@@ -4,28 +4,32 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class N_M7 {
+public class N_M8 {
+	
 	public static int N, M;
 	public static int[] arr;
 	public static int[] res;
 	public static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N];
 		res = new int[M];
 		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
 		Arrays.sort(arr);
-		dfs(0);
+		dfs(0, 0);
 		System.out.println(sb);
 	}
 	
-	public static void dfs(int depth) {
+	public static void dfs (int at, int depth) {
 		if (depth == M) {
 			for (int val : res) {
 				sb.append(val).append(' ');
@@ -34,9 +38,10 @@ public class N_M7 {
 			return ;
 		}
 		
-		for (int i = 0; i < N; i++) {
+		for (int i  = at; i < N; i++) {
 			res[depth] = arr[i];
-			dfs(depth + 1);
+			dfs(i, depth + 1);
 		}
 	}
+
 }
