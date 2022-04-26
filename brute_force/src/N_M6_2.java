4b52fde3edb4e599e3dcb76925bef3bc3aeff8a0
ysb4642 <ysb4642@naver.com>
2022-04-26 21:09:26 +0900

    22_04_26

---
+++ brute_force/src/N_M6_2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N_M6_2 {
+	public static int N, M;
+	public static int[] arr;
+	public static int[] res;
+	public static boolean visited[];
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		res = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		dfs(0, 0);
+		System.out.println(sb);
+
+	}
+	
+	static void dfs(int at, int depth) {
+		if (depth == M) {
+			for (int val : res) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return ;
+		}
+		for (int i = at; i < N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				res[depth] = arr[i];
+				dfs(i + 1, depth + 1);
+				visited[i] = false;
+			}
+		}
+		
+	}
+
+}
